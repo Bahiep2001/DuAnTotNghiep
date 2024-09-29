f5011533eae8dab3ab945663b95a835e29833aa0
Dinh-Tung-246 <145095729+Dinh-Tung-246@users.noreply.github.com>
2024-09-29 09:11:01 +0700

    config frontend to connect css file

---
+++ AutoKid/src/main/java/com/example/demo/TEST/NhanVien.java
@@ -0,0 +1,23 @@
+package com.example.demo.TEST;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Entity(name = "nhanvien")
+@Getter
+@Setter
+public class NhanVien {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idnv")
+    private Integer id;
+
+    @Column(name = "manv")
+    private String maNV;
+
+    @Column(name = "tennv")
+    private String tenNV;
+}
---
+++ AutoKid/src/main/java/com/example/demo/TEST/NhanVienControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.TEST;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NhanVienControllerTest {
+    @Autowired
+    NhanVienRepoTest repo;
+
+    @GetMapping("/nhanvien")
+    public String demo(){
+        return "/ogani-master/index";
+    }
+
+    @GetMapping("/blog")
+    public String demo2(){
+        return  "/ogani-master/blog";
+    }
+}
