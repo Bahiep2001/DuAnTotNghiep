4174e5b4d0a786ed987419af78d1ab4233b479a2
Bahiep2001 <nguyenbahiep27021999@gmail.com>
2024-10-03 17:58:44 +0700

    update imgs

--- AutoKid/src/main/java/com/example/demo/controller/QuanLyDangNhapController.java
+++ AutoKid/src/main/java/com/example/demo/controller/QuanLyDangNhapController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuanLyDangNhapController {
+
+    @RequestMapping ("/home")
+    public String index(){
+        return "home";
+    }
 }
