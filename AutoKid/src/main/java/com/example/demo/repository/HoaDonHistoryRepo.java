3372e4df1cd83a9c601fb52a1b12fe19e0165e26
Dinh-Tung-246 <145095729+Dinh-Tung-246@users.noreply.github.com>
2024-10-07 10:58:38 +0700

    update branch tungsoi

--- AutoKid/src/main/java/com/example/demo/repository/HoaDonHistoryRepo.java
+++ AutoKid/src/main/java/com/example/demo/repository/HoaDonHistoryRepo.java
@@ -2,8 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.HoaDonHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
-
+@Repository
 public interface HoaDonHistoryRepo extends JpaRepository<HoaDonHistory,Integer> {
 }
--- AutoKid/src/main/java/com/example/demo/repository/LoaiSanPhamRepo.java
+++ AutoKid/src/main/java/com/example/demo/repository/LoaiSanPhamRepo.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.LoaiSanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoaiSanPhamRepo extends JpaRepository<LoaiSanPham,Integer> {
 }
